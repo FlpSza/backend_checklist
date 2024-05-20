b05d8fcc5bd913c0ec5a2b89eb1183c56e2b808e
unknown <you@example.com>
2024-05-20 10:47:30 -0300

    tratamento de erro de conexão

--- database.js
+++ database.js
@@ -3,14 +3,33 @@ const mysql = require('mysql');
 
 dotenv.config();
 
-const pool = mysql.createConnection({
+let pool;
+
+function createPool() {
+  pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
     database: process.env.DB,
-    connectionLimit: 0 // limite máximo de conexões simultâneas (0 para ilimitado)
-});
+    connectionLimit: 10 // número máximo de conexões no pool
+  });
 
-// Exporte o pool de conexão para uso em outras partes do aplicativo
+  pool.on('connection', function (connection) {
+    console.log('Nova conexão ao banco de dados foi estabelecida.');
+  });
+
+  pool.on('error', function (err) {
+    console.error('Erro no pool de conexões ao banco de dados:', err);
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+      console.log('Conexão perdida. Tentando reconectar...');
+      createPool(); // Tentar reconectar
+    } else {
+      throw err;
+    }
+  });
+}
+
+createPool();
+
 module.exports = pool;
